aa0bba1ce48756ee30e831b299e7f8ab94dde830
pritha-banerjee1207 <76420944+pritha-banerjee1207@users.noreply.github.com>
2024-02-28 13:52:22 +0530

    Update DynamicTableTest.java

--- Maven_Pro_Table/src/test/java/Caw_studio_Maven/DynamicTableTest.java
+++ Maven_Pro_Table/src/test/java/Caw_studio_Maven/DynamicTableTest.java
@@ -20,7 +20,7 @@ public class DynamicTableTest {
 	private WebDriver driver;
 	private String url = "https://testpages.herokuapp.com/styled/tag/dynamic-table.html";
 	
-	// Sample data
+	
 	private String jsonData = "[{\"name\":\"Bob\",\"age\":20,\"gender\":\"male\"},{\"name\":\"George\",\"age\":42,\"gender\":\"male\"},{\"name\":\"Sara\",\"age\":42,\"gender\":\"female\"},{\"name\":\"Conor\",\"age\":40,\"gender\":\"male\"},{\"name\":\"Jennifer\",\"age\":42,\"gender\":\"female\"}]";
 
 	@Before
